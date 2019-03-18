f2ed1eb1e0a5c989f65d7ab4ae0d80d14a570f21
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-03-18 15:42:40 -0500

    This is a trash pile but I'm trying

---
+++ src/scripts/apiManager.js
@@ -0,0 +1,30 @@
+// const parksCall = (name) => {
+//     fetch("https://localhost:9099/parks")
+//     .then(response => response.json())
+//     .then (results => {
+//         console.log(results.park.name)
+//     })
+
+// }
+
+// console.log(parksCall())
+
+const parksCall = () => {
+    return fetch("http://localhost:9099/parks")
+        .then(response => response.json())
+        .then(parsed => console.log(parsed))
+
+    parsed.forEach(park => {
+        displayInfo.innerHTML = key.name
+    })
+}
+
+
+console.log(parksCall())
+
+// .then(results => 
+//         {console.log(results.name, results.state)
+//         results.name.forEach(park => {
+//         displayInfo(parks.name, parks.state)
+//         })
+//     })
---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+console.log("You're doing great, son!")
+
+// Process: get results from API
+
+const displayInfo = document.querySelector("#info")
+
